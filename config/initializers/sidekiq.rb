550c35c3436ccd212390e951c6bc228151ee2931
Isaac Alencar <alencarisaac3@gmail.com>
2025-01-03 15:07:59 -0300

    change redis url to env variables

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,11 +1,13 @@
 require 'sidekiq/cron/job'
 
+redis_url = ENV["REDIS_URL"]
+
 Sidekiq.configure_server do |config|
-  config.redis = { url: "redis://redis:6379/0" }
+  config.redis = redis_url
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: "redis://redis:6379/0" }
+  config.redis = redis_url
 end
 
 schedule_file = "config/schedule.yml"
